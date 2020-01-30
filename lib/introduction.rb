4e54238bfa5b1b0d0ec8e29a373b17be903bccc6
Ricky Rozier <rickydrozier@gmail.com>
2020-01-30 04:03:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 # lib/introduction.rb
  
-def introduction_with_language ricky,ruby
+def introduction(ricky) + language(ruby)
   puts "Hi, my name is #{ricky} and I am learning to program in #{ruby}."
 end
